4dd9c3090d57d01fe368dff2a787091ee76f4e93
Benjamin Howe <yefool@hotmail.co.uk>
2023-07-28 17:16:21 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { evaluate } from "mathjs";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const oprtnBtn = ["+", "-", "*", "/", "=", "C",];
+  const clearEntry = ["CE"]
+  const [result, setResult] = useState("");
+
+
+  const button = (input) => {
+    input === "C" ? setResult("") : input === "=" ? setResult(evaluate(result)) : setResult(result + input)
+    
+  }
+
+  // const clear = (index) => {
+  //   let storedNums = [...numbers]
+  //   storedNums.splice(index, -1);
+  //   setResult(storedNums)
+  // }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="box">
+      <div className="result">
+        <p>{result}</p>
+      </div>
+
+      
+        <div className="numbers"> 
+          {numbers.map((index) => {
+            return (
+              <div key={index}>
+                <button onClick={() => button(index)}>{index}</button>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="operations">
+          {oprtnBtn.map((index) => {
+            return (
+              <button className="ops" onClick={() => button(index)} key={index}>
+                {index}
+              </button>
+            );
+          })}
+          {clearEntry.map((index) => {
+            return (
+              <button className="clear" onClick={() => button(index)} key={index}>
+                {index}
+              </button> 
+               );
+              })}
+        </div>
+        </div>
+  
   );
-}
+};
 
 export default App;
